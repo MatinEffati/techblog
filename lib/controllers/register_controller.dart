8b13c76e7232131152e46e8efa04f3af0986edb3
MatinEffati <telecleverguy@gmail.com>
2022-09-17 22:21:32 +0430

    token saved with get storage

--- lib/controllers/register_controller.dart
+++ lib/controllers/register_controller.dart
@@ -1,11 +1,17 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:tech_blog/components/api_constants.dart';
+import 'package:tech_blog/components/storage_const.dart';
 import 'package:tech_blog/services/dio_service.dart';
+import 'package:tech_blog/ui/main_screens/main_screen.dart';
 
 class RegisterController extends GetxController {
   TextEditingController emailTextEditingController = TextEditingController();
-  TextEditingController activateCodeTextEditingController = TextEditingController();
+  TextEditingController activateCodeTextEditingController =
+      TextEditingController();
   var email = '';
   var userId = '';
 
@@ -15,20 +21,33 @@ class RegisterController extends GetxController {
       'command': 'register',
     };
 
-    var response = await DioService().postMethod(map, ApiConstants.postRegister);
+    var response =
+        await DioService().postMethod(map, ApiConstants.postRegister);
 
     email = emailTextEditingController.text;
     userId = response.data['user_id'];
   }
 
-  verify()async{
+  verify() async {
     Map<String, dynamic> map = {
       'email': email,
-      'user_id':userId,
-      'code':activateCodeTextEditingController.text,
+      'user_id': userId,
+      'code': activateCodeTextEditingController.text,
       'command': 'verify',
     };
 
     var response = await DioService().postMethod(map, ApiConstants.postRegister);
+    if(response.data['response'] == 'verified'){
+      var box = GetStorage();
+      box.write(token, response.data['token']);
+      box.write(userIdKey, response.data['user_id']);
+
+      print('read -> ${box.read(token)}');
+      print('read -> ${box.read(userIdKey)}');
+      Get.to(MainScreen());
+    }else{
+      log('error');
+    }
+
   }
 }
